5df07726a6f9352e86ef2957427a35b85c7a5061
lucasmebo <lucasmebo@hotmail.com>
2025-04-28 16:34:38 -0300

    Ci-cd

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
-    baseUrl: 'https://golden-movie-studio.vercel.app/', //url PRD
+    baseUrl: 'https://golden-movie-studio.vercel.app/',
     video: true
   },
 });
