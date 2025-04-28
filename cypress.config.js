0b59eedc630cd2d13aaeaa68b0255662184ad2dd
lucasmebo <lucasmebo@hotmail.com>
2025-04-28 16:02:39 -0300

    Atualizando a config de url de produção

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://golden-movie-studio.vercel.app/', //Url de produção
-    //baseUrl: 'http://127.0.0.1:8080/',  //Url local
+    baseUrl: 'https://golden-movie-studio.vercel.app/',
     video: true
   },
 });
